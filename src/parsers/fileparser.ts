1e87f75206f40c155f18c65e066fd1a8c039c001
vikramsamak <vikramsamak02@gmail.com>
2025-03-12 21:12:12 +0530

    feat: enhance fileParser to include component code snippets and file extension in output

--- src/parsers/fileparser.ts
+++ src/parsers/fileparser.ts
@@ -1,13 +1,16 @@
 import fs from 'fs';
+import path from 'path';
 import { parse } from '@babel/parser';
 import traverse, { NodePath } from '@babel/traverse';
 import * as t from '@babel/types';
+import { extractProps } from './parserutils';
+import { extractJsDoc } from './parserutils';
+import { generateCodeSnippet } from './parserutils';
 import { ComponentInfo } from '../types';
-import { extractProps } from './parserutils/extractprops';
-import { extractJsDoc } from './parserutils/extractjsdocs';
 
 export function fileParser(filePath: string): ComponentInfo[] {
   const code = fs.readFileSync(filePath, 'utf-8');
+  const fileExtension = path.extname(filePath);
   const ast = parse(code, {
     sourceType: 'module',
     plugins: ['jsx', 'typescript'],
@@ -26,9 +29,33 @@ export function fileParser(filePath: string): ComponentInfo[] {
               t.isFunctionExpression(declaration.init))
           ) {
             const componentName = declaration.id.name;
-            const props = extractProps(declaration.init.params);
+            const params = declaration.init.params.filter(
+              (
+                param
+              ): param is
+                | t.Identifier
+                | t.ObjectPattern
+                | t.ArrayPattern
+                | t.AssignmentPattern
+                | t.RestElement =>
+                t.isIdentifier(param) ||
+                t.isObjectPattern(param) ||
+                t.isArrayPattern(param) ||
+                t.isAssignmentPattern(param) ||
+                t.isRestElement(param)
+            );
+
+            const props = extractProps(params);
             const jsDoc = extractJsDoc(path);
-            components.push({ componentName, props, jsDoc });
+            const componentCode = generateCodeSnippet(path.node);
+
+            components.push({
+              componentName,
+              props,
+              jsDoc,
+              code: componentCode,
+              fileExtension
+            });
           }
         });
       }
@@ -36,9 +63,27 @@ export function fileParser(filePath: string): ComponentInfo[] {
     FunctionDeclaration(path: NodePath<t.FunctionDeclaration>) {
       if (path.node.id) {
         const componentName = path.node.id.name;
-        const props = extractProps(path.node.params);
+        const params = path.node.params.filter(
+          (
+            param
+          ): param is
+            | t.Identifier
+            | t.ObjectPattern
+            | t.ArrayPattern
+            | t.AssignmentPattern
+            | t.RestElement =>
+            t.isIdentifier(param) ||
+            t.isObjectPattern(param) ||
+            t.isArrayPattern(param) ||
+            t.isAssignmentPattern(param) ||
+            t.isRestElement(param)
+        );
+
+        const props = extractProps(params);
         const jsDoc = extractJsDoc(path);
-        components.push({ componentName, props, jsDoc });
+        const componentCode = generateCodeSnippet(path.node);
+
+        components.push({ componentName, props, jsDoc, code: componentCode ,fileExtension});
       }
     },
     VariableDeclaration(path: NodePath<t.VariableDeclaration>) {
@@ -50,9 +95,27 @@ export function fileParser(filePath: string): ComponentInfo[] {
             t.isFunctionExpression(declaration.init))
         ) {
           const componentName = declaration.id.name;
-          const props = extractProps(declaration.init.params);
+          const params = declaration.init.params.filter(
+            (
+              param
+            ): param is
+              | t.Identifier
+              | t.ObjectPattern
+              | t.ArrayPattern
+              | t.AssignmentPattern
+              | t.RestElement =>
+              t.isIdentifier(param) ||
+              t.isObjectPattern(param) ||
+              t.isArrayPattern(param) ||
+              t.isAssignmentPattern(param) ||
+              t.isRestElement(param)
+          );
+
+          const props = extractProps(params);
           const jsDoc = extractJsDoc(path);
-          components.push({ componentName, props, jsDoc });
+          const componentCode = generateCodeSnippet(path.node);
+
+          components.push({ componentName, props, jsDoc, code: componentCode ,fileExtension});
         }
       });
     },
