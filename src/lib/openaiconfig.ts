f61b376a618397ccf2d50c782e9629d90660d164
vikramsamak <vikramsamak02@gmail.com>
2025-03-16 13:59:41 +0530

    feat: add dotenv package for environment variable management and update OpenAI config to use environment variable for API key

--- src/lib/openaiconfig.ts
+++ src/lib/openaiconfig.ts
@@ -2,5 +2,5 @@ import OpenAI from 'openai';
 
 export const openai = new OpenAI({
   baseURL: 'https://openrouter.ai/api/v1',
-  apiKey: '<OPENROUTER_API_KEY>',
+  apiKey: process.env.OPEN_ROUTER_API_KEY as string,
 });
