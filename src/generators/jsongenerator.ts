a1ff71d35c16e39f8c1c6de56a0cb564fd775c95
vikramsamak <vikramsamak02@gmail.com>
2025-03-16 11:39:29 +0530

    feat: enhance jsonGenerator to include metadata for file extensions, component names, and used JSDoc tags

--- src/generators/jsongenerator.ts
+++ src/generators/jsongenerator.ts
@@ -4,11 +4,27 @@ import prettier from 'prettier';
 export async function jsonGenerator(
   components: ComponentInfo[]
 ): Promise<string> {
+  const allExtensions = Array.from(
+    new Set(components.map((c) => c.fileExtension))
+  );
+
+  const allComponentNames = components.map((c) => c.componentName);
+
+  const allTags = new Set<string>();
+  components.forEach((comp) => {
+    comp.jsDoc?.tags?.forEach((tag) => {
+      allTags.add(tag.title);
+    });
+  });
+
   const rawJson = JSON.stringify(
     {
       metadata: {
         generatedAt: new Date().toISOString(),
         componentCount: components.length,
+        fileExtensions: allExtensions,
+        componentNames: allComponentNames,
+        tagsUsed: Array.from(allTags),
       },
       components,
     },
