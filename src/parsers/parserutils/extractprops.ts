ffb152ec452863fd7cd20da87a5756cd448c35f5
vikramsamak <vikramsamak02@gmail.com>
2025-03-16 11:14:14 +0530

    feat: enhance extractProps function to support rest elements and assignment patterns

--- src/parsers/parserutils/extractprops.ts
+++ src/parsers/parserutils/extractprops.ts
@@ -12,12 +12,35 @@ export function extractProps(
   return params
     .map((param) => {
       if (t.isObjectPattern(param)) {
-        return param.properties.map((prop) =>
-          t.isObjectProperty(prop) && t.isIdentifier(prop.key)
-            ? prop.key.name
-            : null
-        );
+        // { a, b }
+        return param.properties.map((prop) => {
+          if (t.isObjectProperty(prop) && t.isIdentifier(prop.key)) {
+            return prop.key.name;
+          } else if (t.isRestElement(prop) && t.isIdentifier(prop.argument)) {
+            return `...${prop.argument.name}`;
+          }
+          return null;
+        });
+      } else if (t.isArrayPattern(param)) {
+        // [a, b]
+        return param.elements.map((el) => {
+          if (t.isIdentifier(el)) {
+            return el.name;
+          } else if (t.isRestElement(el) && t.isIdentifier(el.argument)) {
+            return `...${el.argument.name}`;
+          } else if (t.isAssignmentPattern(el) && t.isIdentifier(el.left)) {
+            return el.left.name;
+          }
+          return null;
+        });
+      } else if (t.isAssignmentPattern(param) && t.isIdentifier(param.left)) {
+        // param = defaultValue
+        return param.left.name;
+      } else if (t.isRestElement(param) && t.isIdentifier(param.argument)) {
+        // ...args
+        return `...${param.argument.name}`;
       } else if (t.isIdentifier(param)) {
+        // normal param
         return param.name;
       }
       return null;
